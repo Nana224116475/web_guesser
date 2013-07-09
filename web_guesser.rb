842ab59573c024bd5ed519fec936ac52cebabd80
Don Nana <nana15@validusprep.org>
2013-07-09 16:50:27 -0400

    Got to the part of guessing the number

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,7 +3,21 @@ require 'sinatra/reloader'
 
 number = (1 + rand(99)).to_s
 
+
 get "/" do
-	erb :index, :locals => {:number => number}
+	message = check_guess(params["guess"])
+	puts message
+	erb :index, :locals => {:number => number, :message => message} 
 end
- 
+
+ def check_guess(guess)
+ 	if number.to_i + 5 <= guess.to_i
+ 		return "TOO HIGH"
+ 	elsif number.to_i - 5 >= guess.to_i
+ 		return "TOO LOW"
+ 	elsif number ==  guess.to_i
+ 		return "You guessed it correctly"
+ 	elsif true
+ 	  return "TRUE"
+ 	end
+ end
